f0ee9cf78ed8a4dde3d14fd7656fd523b0fbc7f9
Quinten Ballard <uw94057@umbc.edu>
2026-06-28 11:08:44 -0400

    created basic raw data pulls

--- data/raw.py
+++ data/raw.py
@@ -1,29 +1,71 @@
 import requests
+import os
+from dotenv import load_dotenv
 from pyspark.sql import SparkSession
 
-url = "https://data.cityofchicago.org/resource/ezma-pppn.json"
+def fetch_business_owner_data() -> list:
+    """
+    Fetches business owner data from the Chicago Data Portal API.
 
+    Returns:
+        list: A list of dictionaries containing business owner data.
+    """
+    url = os.getenv("BUSINESSOWNERSURL")
 
-params = {
-    "$limit": 1000,
-    "$offset": 0
-}
+    params = {
+        "$limit": 1000,
+        "$offset": 0
+    }
 
-results = []
+    results = []
+    while True:
+        response = requests.get(url, params=params, timeout=30)
+        response.raise_for_status()
 
-while True:
-    response = requests.get(url, params=params, timeout=30)
-    response.raise_for_status()
+        data = response.json()
+        results.extend(data)
+        if len(data) < params["$limit"]:
+            break
+        params["$offset"] += params["$limit"]
+    return results
 
-    data = response.json()
-    print(data)
-    results.extend(data)
-    if len(data) < params["$limit"]:
-        break
-    params["$offset"] += params["$limit"]
+def fetch_business_license_data() -> list:
+    """
+    Fetches business license data from the Chicago Data Portal API.
 
-spark = SparkSession.builder.appName("Chicago Data").getOrCreate()
+    Returns:
+        list: A list of dictionaries containing business license data.
+    """
+    #url = os.getenv("BUSINESSLICENSEURL")
+    url = os.getenv("BUSINESSLICENSEURL")
+    params = {
+        "$limit": 1000,
+        "$offset": 0
+    }
 
+    results = []
+    while True:
+        response = requests.get(url, params=params, timeout=30)
+        response.raise_for_status()
 
-df = spark.createDataFrame(results)
-df.show()
+        data = response.json()
+        print(data)
+        results.extend(data)
+        if len(data) < params["$limit"] or len(data) == 0:
+            break
+        params["$offset"] += params["$limit"]
+    return results
+
+def main():
+    load_dotenv()
+    business_owner_data = fetch_business_owner_data()
+    business_license_data = fetch_business_license_data()
+    
+    spark = SparkSession.builder.appName("Chicago Data").getOrCreate()
+
+    df = spark.createDataFrame(business_license_data)
+    df.show()   
+
+if __name__ == "__main__":
+    main()
+
